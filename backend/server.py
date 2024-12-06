02c7a2549e4ca7f4e82ccb9fe489f8c8eeba1990
Konrad" "Rozpadek <rozpadekk@dh-node14.hpc.msoe.edu>
2024-12-05 21:05:03 -0500

    cleaned up filed

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,19 @@
 from flask import Flask,render_template,request
 from flask_socketio import SocketIO, emit
 import subprocess
-from flask_sock import Sock 
+import socket
+import time
+import json
+from flask import send_from_directory
+from flask import session
+
+import sys
+sys.path.append('../llm')
 import llm as L
-#    print(f"ssh -L 8001:{host}:8001 {getpass.getuser()}@dh-mgmt2.hpc.msoe.edu")
 
 app = Flask(__name__)
-socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='eventlet')
-sock = Sock(app)
-
+socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='threading')
+L.LLM("")
 
 def logits_processor(prev_tok_ids, next_tok_logits):
     return next_tok_logits
@@ -17,11 +22,41 @@ def logits_processor(prev_tok_ids, next_tok_logits):
 def main():
         return "test"
 
-@sock.route('/chat/ws')
-def echo(ws):
-    llm = L.LLM('You are a British butler. Start every answer with "you"')
-    while True:
-        data = ws.receive()
-        resp = llm(data, max_tokens=20, logits_processor=[logits_processor])
-        ws.send(resp)
-app.run(port=8081)
+@app.route('/static/<path:path>')
+def static_get(path):
+    # Using request args for path will expose you to directory traversal attacks
+    return send_from_directory('static', path)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on("identify")
+def identify(identity):
+    session['identity'] = identity
+
+@socketio.on("french")
+def french(prompt):
+    if 'french' not in session:
+        session['french'] = L.LLM('You are a storyteller who speaks in french')
+
+    french = session['french']
+    
+    print(f"{session['identity']} sent : {prompt}")
+    resp = french(prompt, response_format='stream', max_tokens=200, logits_processor=[logits_processor])
+    emit("french", "<START>") 
+    for token in resp:
+        emit("french", token)
+    emit("french", "<END>") 
+
+
+@socketio.on("new-french")
+def new_french():
+    french = L.LLM('You are a storyteller who speaks in french')
+    session['french'] = french
+
+
+
+print(socket.gethostname())
+
+app.run(host=socket.gethostname(), port=8001)
