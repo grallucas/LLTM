b037f497a643505f038e7e4faebaf0a1e0eab573
grallucas <grall@msoe.edu>
2025-03-20 22:29:53 -0500

    impl v1 in-ctx translation

---
+++ app/translate.py
@@ -0,0 +1,39 @@
+import lexicon
+import llm as L
+
+def format_word_data(word_data):
+    word_data_fmted = ''
+
+    for word_type, defs in word_data['definitions'].items():
+        word_data_fmted += word_type + '\n'
+        for d in defs:
+            word_data_fmted += f'\t{d[0]}\n'
+
+    return word_data_fmted
+
+def translate_in_ctx(context, word, word_lexicon_data):
+    if word_lexicon_data:
+        word_lexicon_data = format_word_data(word_lexicon_data)
+
+    llm = L.LLM('You are an Finnish -> English translator.')
+
+    translated_word = llm(
+        f'{ctx}' +
+        f'\n\nGiven the prior context, translate the Finnish word "{word}" into english' + 
+        (f' from one of these definitions:\n{word_data}' if word_data else '.')
+        ,
+        response_format=['english_word']
+    )['english_word']
+
+    explanation = llm(
+        'Give a brief, one-sentence explanation generalizable to other sentences (i.e., your explanation should be unconnected from the context).',
+        response_format=['concise_generalizable_explanation']
+    )['concise_generalizable_explanation']
+
+
+    breakdown = llm(
+        'Break the word into its morphemes if applicable.',
+        response_format=['parts_or_n/a']
+    )['parts_or_n/a']
+
+    return translated_word, explanation, breakdown
